9cadfffc982cf68fb95e700c70003fa554f50173
Noah Lipsyc <nlipsyc@gmail.com>
2015-10-19 16:36:53 -0400

    Green button starts prog.  Also stops and resets to default.

--- easel-source.js
+++ easel-source.js
@@ -1,128 +1,146 @@
 //Constants
-var STAGE = {width: 800, height: 500},
-	PLAYGROUND = {width : 500, height: 500},
-	CONTROLPANEL = {width: 300, height: 500},
-	GRID = {col: 10, row: 10},
-	UNIT = {x: PLAYGROUND.width/GRID.col, y: PLAYGROUND.height/GRID.row},
+var STAGE = {
+		width: 800,
+		height: 500
+	},
+	PLAYGROUND = {
+		width: 500,
+		height: 500
+	},
+	CONTROLPANEL = {
+		width: 300,
+		height: 500
+	},
+	GRID = {
+		col: 10,
+		row: 10
+	},
+	UNIT = {
+		width: PLAYGROUND.width / GRID.col,
+		height: PLAYGROUND.height / GRID.row
+	},
 	stage, bot,
 	ticker = createjs.Ticker;
 
 
-	
-function init(){
+
+function init() {
 	stage = new createjs.Stage("demoCanvas");
 
 	//Allows mouse tracking outside of the stage
-	stage.mouseMoveOutside = true; 
+	stage.mouseMoveOutside = true;
 
 	stage.enableMouseOver();
 
 	bot = new createjs.Shape();
-	bot.graphics.beginFill("gray").drawRoundRect(0, 0, UNIT.x*0.8, UNIT.y*0.8, 5);
-	bot.x = CONTROLPANEL.width;
-	bot.y = STAGE.height-UNIT.y;
+	bot.graphics.beginFill("gray").drawRoundRect(0, 0, UNIT.width * 0.8, UNIT.height * 0.8, 5);
+	bot.defaultPos = {x: CONTROLPANEL.width, y: STAGE.height - UNIT.height};
+	bot.x = bot.defaultPos.x;
+	bot.y = bot.defaultPos.y;
+	bot.orientation = "e";
+	bot.moving = false;
+	bot.moveForward = function(dist) {
+		handleMoveForward(dist, bot.orientation);
+	};
 	stage.addChild(bot);
 
+	goTime = new createjs.Shape();
+	goTime.graphics.beginFill("green").dc(0, 0, UNIT.width);
+	goTime.x = STAGE.width - UNIT.height;
+	goTime.y = UNIT.height;
+	goTime.progRunning = false;
+	stage.addChild(goTime);
 	// line = new createjs.Shape();
 	// stage.addChild(line);
 
 	stage.update();
 
-//Mouse events
+	//Listeners
 
-	bot.on("pressmove", function(evt){
-		evt.currentTarget.x = handleXSnap(evt.stageX);
-		evt.currentTarget.y = handleYSnap(evt.stageY);
+	bot.on("pressmove", function(evt) {
+				evt.currentTarget.x = handleXSnap(evt.stageX);
+				evt.currentTarget.y = handleYSnap(evt.stageY);
 
-	
-	function handleXSnap(cursorPos){
-		if(cursorPos>CONTROLPANEL.width){ //Make sure we're still in the playground
-			return Math.round((cursorPos-UNIT.x/2)/UNIT.x)*UNIT.x; //Snaps to closest gird point.
+			});
+
+	goTime.on("click", function() {
+		console.log('click');
+		handleGoTime();
+	});
+
+			ticker.addEventListener("tick", tick);
+		
+		//Handlers
+
+	function handleXSnap(cursorPos) {
+		if (cursorPos > CONTROLPANEL.width) { //Make sure we're still in the playground
+			return Math.round((cursorPos - UNIT.width / 2) / UNIT.width) * UNIT.width; //Snaps to closest gird point.
 		}
 		return CONTROLPANEL.width;
-		}	
-	
-	function handleYSnap(cursorPos){
-// console.log(cursorPos, Math.round((cursorPos-UNIT.y/2)/UNIT.y)*UNIT.y);
+	}
 
-			return Math.round((cursorPos-UNIT.y/2)/UNIT.y)*UNIT.y; //Snaps to closest gird point.
-		}	
-	
+	function handleYSnap(cursorPos) {
+		// console.log(cursorPos, Math.round((cursorPos-UNIT.height/2)/UNIT.height)*UNIT.height);
+
+		return Math.round((cursorPos - UNIT.hitTestght / 2) / UNIT.height) * UNIT.height; //Snaps to closest gird point.
+	}
+
+	function handleMoveForward(dist, ori){
+		switch (ori){
+			case "e":
+				bot.x += dist;
+				break;
+
+			case "s":
+				bot.y += dist;
+				break;
+
+			case "w":
+				bot.x -= dist;
+				break;
+
+			case "n":
+				bot.y -= dist;
+				break;
+
+			default:
+				console.log("Error: Bot has invalid orientation property");
+
+		}
+			}
+
+	function handleGoTime() {
+		if (!goTime.progRunning) { //We start the program
+			bot.moving = true;
+		} else { //We end the program and reset the field
+			bot.moving = false;
+			setTimeout(function() {			
+			bot.x = bot.defaultPos.x;
+			bot.y = bot.defaultPos.y;}, 
+			250);
+		}
+
+		goTime.progRunning = !goTime.progRunning;
+	}
+
 	stage.update();
-	});
-		// circle.on("click", function(){
-		// 	if(circle.x%2===0){
-		// 		circle.x+=51;
-		// 	}
-		// 	else{
-		// 		circle.x-=51;
-		// 	}
-		// 	stage.update();
-		// });
-		// circle.on("mouseover", function(event){
-		// 	console.log("mouseover");
-		// 	circle.graphics.clear().beginFill("purple").drawCircle(0,0,50);
-		// 	stage.update();
-		// });
-		// circle.on("mouseout", function(event){
-		// 	console.log("mouseout");
-		// 	circle.graphics.clear().f("DeepSkyBlue").dc(0,0,50);
-		// 	stage.update();
-		// });
-
-	// square.on("pressmove", function(evt){
-	// 	evt.currentTarget.x = evt.stageX;
-	// 	evt.currentTarget.y = evt.stageY;
-
-	// 	stage.update();
-	// });
-	// circle.on("click", function(event){
-	// 	console.log("click!");
-	// 	strokeColour = createjs.Graphics.getHSL(Math.random()*360, 100, 50);
-	// 	strokeWeight = 20*Math.random();
-
-	// 	handleDraw();
-	// });
-
-
-	ticker.addEventListener("tick", tick);
 }
 
-
-// /**********Drawing*******/
-
-
-	// function handleDraw(){
-	// 	drawShown=!drawShown;
-
-	// 	console.log(drawShown);
-
-	// 	stage.on("stagemousemove", function(evt){
-
-	// 		if(oldX && drawShown){
-	// 			line.graphics.beginStroke(strokeColour)
-	// 							.setStrokeStyle(strokeWeight,"round")
-	// 							.moveTo(oldX,oldY)
-	// 							.lineTo(evt.stageX, evt.stageY);
-	// 			stage.update();
-	// 		}
-	// 		oldX = evt.stageX;
-	// 		oldY = evt.stageY;
-	// 	});
-	// 	stage.update();
-	// }
-
 /***********Animation************/
-function tick(event){
-			//ms since last tick / ms per s * px per sec
+function tick(event) {
+	//ms since last tick / ms per s * px per sec
+	if(bot.moving && bot.x<STAGE.width-UNIT.width){
+		bot.moveForward(event.delta / 1000 * 200);
+	}	
+
 
 	//Hit test
-	
+
 	// var pt = square.localToLocal(12, 12, circle);
 
 	// console.log("square", pt.x, pt.y);
 	// console.log("circle", circle.x, circle.y);
-	
+
 	// if (circle.hitTest(pt.x, pt.y)){
 	// 	console.log('hit');
 	// 	circle.alpha = 0.5;
@@ -130,4 +148,3 @@ function tick(event){
 
 	stage.update();
 }
-	
