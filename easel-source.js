2b23bb67e5f6f6a1572dd9d3faa1e5bab1518b47
Noah Lipsyc <nlipsyc@gmail.com>
2015-10-19 14:18:20 -0400

    Bot now snaps to grid

--- easel-source.js
+++ easel-source.js
@@ -1,7 +1,11 @@
-// window.alert("working!");
-var stage, circle, oldX, oldY, strokeColour, strokeWeight,
-	ticker = createjs.Ticker,
-	drawShown = false;
+//Constants
+var STAGE = {width: 800, height: 500},
+	PLAYGROUND = {width : 500, height: 500},
+	CONTROLPANEL = {width: 300, height: 500},
+	GRID = {col: 10, row: 10},
+	UNIT = {x: PLAYGROUND.width/GRID.col, y: PLAYGROUND.height/GRID.row},
+	stage, bot,
+	ticker = createjs.Ticker;
 
 
 	
@@ -13,28 +17,39 @@ function init(){
 
 	stage.enableMouseOver();
 
+	bot = new createjs.Shape();
+	bot.graphics.beginFill("gray").drawRoundRect(0, 0, UNIT.x*0.8, UNIT.y*0.8, 5);
+	bot.x = CONTROLPANEL.width;
+	bot.y = STAGE.height-UNIT.y;
+	stage.addChild(bot);
 
-	circle = new createjs.Shape();
-	circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
-	circle.x = 100;
-	circle.y = 100;
-	stage.addChild(circle);
-
-	square = new createjs.Shape();
-	square.graphics.beginFill("green").drawRect(0,0,25,25);
-	square.x = 0;
-	square.y = 0;
-	stage.addChild(square);
-
-
-	line = new createjs.Shape();
-	stage.addChild(line);
+	// line = new createjs.Shape();
+	// stage.addChild(line);
 
 	stage.update();
 
 //Mouse events
 
+	bot.on("pressmove", function(evt){
+		evt.currentTarget.x = handleXSnap(evt.stageX);
+		evt.currentTarget.y = handleYSnap(evt.stageY);
 
+	
+	function handleXSnap(cursorPos){
+		if(cursorPos>CONTROLPANEL.width){ //Make sure we're still in the playground
+			return Math.round((cursorPos-UNIT.x/2)/UNIT.x)*UNIT.x; //Snaps to closest gird point.
+		}
+		return CONTROLPANEL.width;
+		}	
+	
+	function handleYSnap(cursorPos){
+// console.log(cursorPos, Math.round((cursorPos-UNIT.y/2)/UNIT.y)*UNIT.y);
+
+			return Math.round((cursorPos-UNIT.y/2)/UNIT.y)*UNIT.y; //Snaps to closest gird point.
+		}	
+	
+	stage.update();
+	});
 		// circle.on("click", function(){
 		// 	if(circle.x%2===0){
 		// 		circle.x+=51;
@@ -55,12 +70,12 @@ function init(){
 		// 	stage.update();
 		// });
 
-	square.on("pressmove", function(evt){
-		evt.currentTarget.x = evt.stageX;
-		evt.currentTarget.y = evt.stageY;
+	// square.on("pressmove", function(evt){
+	// 	evt.currentTarget.x = evt.stageX;
+	// 	evt.currentTarget.y = evt.stageY;
 
-		stage.update();
-	});
+	// 	stage.update();
+	// });
 	// circle.on("click", function(event){
 	// 	console.log("click!");
 	// 	strokeColour = createjs.Graphics.getHSL(Math.random()*360, 100, 50);
@@ -71,7 +86,6 @@ function init(){
 
 
 	ticker.addEventListener("tick", tick);
-	ticker.setFPS(20);
 }
 
 
@@ -101,31 +115,18 @@ function init(){
 /***********Animation************/
 function tick(event){
 			//ms since last tick / ms per s * px per sec
-	circle.alpha = 1;
-	circle.x += event.delta/1000*10;
-	if(circle.x>stage.canvas.width){
 
-
-
-		circle.x=0;
-			//increases fps with each cycle
-			//ticker.setFPS(ticker.getFPS()*1.5);
-
-			// if(ticker.getFPS()>80){
-				// ticker.setFPS(20);
-		// }
-	}
 	//Hit test
 	
-	var pt = square.localToLocal(12, 12, circle);
+	// var pt = square.localToLocal(12, 12, circle);
 
-	console.log("square", pt.x, pt.y);
-	console.log("circle", circle.x, circle.y);
+	// console.log("square", pt.x, pt.y);
+	// console.log("circle", circle.x, circle.y);
 	
-	if (circle.hitTest(pt.x, pt.y)){
-		console.log('hit');
-		circle.alpha = 0.5;
-	}
+	// if (circle.hitTest(pt.x, pt.y)){
+	// 	console.log('hit');
+	// 	circle.alpha = 0.5;
+	// }
 
 	stage.update();
 }
